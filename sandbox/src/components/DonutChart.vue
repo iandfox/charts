ed73e4ea34632ae930371b8fa25293eee22a88d8
Ian Fox <iandouglasfox@gmail.com>
2025-07-18 12:11:06 -0700

    feat(DonutChart): Add hover interaction and labels to segments

--- sandbox/src/components/DonutChart.vue
+++ sandbox/src/components/DonutChart.vue
@@ -13,10 +13,24 @@
 						xmlns="http://www.w3.org/2000/svg"
 						:d="segment.toSVGPathDefinition({
 							rotation: toValue(globalRotation),
-							offset: toValue(globalOffset)
+							offset: toValue(globalOffset) + (segment.isHovering ? 2 : 0),
 						})"
 						:fill="segment.color"
+						@mouseover="setHovering(segment, true)"
+						@mouseout="setHovering(segment, false)"
 					/>
+					
+					<text
+						:x="segment.getPoint(0.5, 0.5, { offset: toValue(globalOffset) + (segment.isHovering ? 2 : 0) }).x"
+						:y="segment.getPoint(0.5, 0.5, { offset: toValue(globalOffset) + (segment.isHovering ? 2 : 0) }).y"
+						text-anchor="middle"
+						dominant-baseline="middle"
+						font-size="5"
+						fill="white"
+						style="pointer-events: none; user-select: none;"
+					>
+						{{ segment.label }}: {{ segment.value }}
+					</text>
 				</g>
 			</svg>
 			
@@ -44,14 +58,14 @@
 </template>
 
 <script setup>
-	import { ref, toValue, watch } from 'vue';
+	import { reactive, ref, toValue, watch } from 'vue';
 	import DonutSVG from '../../../src/DonutSVG.class.js';
 	
 	const r0 = ref(10);
 	const globalRotation = ref(0);
 	const globalOffset = ref(0);
 	
-	const donut = new DonutSVG({
+	const donut = reactive(new DonutSVG({
 		data: [
 			{
 				label: 'Pi Charts',
@@ -75,7 +89,11 @@
 				r1: 50,
 			},
 		]
-	});
+	}));
+	
+	const setHovering = (segment, value) => {
+		segment._isHovering = value;
+	}
 	
 	
 	watch(r0, (newValue) => {
